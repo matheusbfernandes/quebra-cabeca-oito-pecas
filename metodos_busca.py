785c380fd296b2972fe594800adf3abfd2d7ea48
Matheus Fernandes <matheusfernandes1981@hotmail.com>
2019-04-23 20:22:45 -0300

    A* terminada

--- metodos_busca.py
+++ metodos_busca.py
@@ -3,23 +3,26 @@ from queue import PriorityQueue
 
 
 class No(object):
-    def __init__(self, tabuleiro, no_pai, c_n, h_n=0):
+    def __init__(self, tabuleiro, no_pai, custo_parcial, custo_huristico=0):
         self.tabuleiro = tabuleiro
         self.no_pai = no_pai
-        self.c_n = c_n
-        self.h_n = h_n
-        self.c_total = self._computar_custo_total()
-
-    def _computar_custo_total(self):
-        if self.no_pai is None:
-            return self.c_n
-        return self.no_pai.c_total + self.c_n
+        self.custo_parcial = custo_parcial
+        self.custo_huristico = custo_huristico
+        self.custo_total = self._computar_custo()
 
     def __lt__(self, other):
-        return self.c_total < other.c_total
+        return self.custo_total < other.custo_total
 
     def __eq__(self, other):
-        return self.c_total == other.c_total
+        return self.custo_total == other.custo_total
+
+    def _computar_custo_total(self):
+        return self.custo_parcial + self.custo_huristico
+
+    def _computar_custo(self):
+        if self.no_pai is None:
+            return self.custo_parcial
+        return self.no_pai.custo_total + self.custo_parcial
 
 
 class BuscaCustoUniforme(object):
@@ -32,7 +35,7 @@ class BuscaCustoUniforme(object):
 
         self.visitados = []
         self.nao_visitados = PriorityQueue()
-        self.nao_visitados.put((no_inicial.c_n, no_inicial))
+        self.nao_visitados.put((no_inicial.custo_parcial, no_inicial))
 
     def _final_jogo(self, tabuleiro_atual):
         return np.all(self.TABULEIRO_FINAL == tabuleiro_atual)
@@ -49,28 +52,28 @@ class BuscaCustoUniforme(object):
             temp[vazio_i[0]][vazio_j[0]] = temp[vazio_i[0] - 1][vazio_j[0]]
             temp[vazio_i[0] - 1][vazio_j[0]] = 0
             novo_no = No(temp, estado_atual, self._calcular_custo(temp))
-            no_filhos.put((novo_no.c_n, novo_no))
+            no_filhos.put((novo_no.custo_parcial, novo_no))
 
         if (vazio_i + 1) < self.TABULEIRO_FINAL.shape[0]:
             temp = np.copy(estado_atual.tabuleiro)
             temp[vazio_i[0]][vazio_j[0]] = temp[vazio_i[0] + 1][vazio_j[0]]
             temp[vazio_i[0] + 1][vazio_j[0]] = 0
             novo_no = No(temp, estado_atual, self._calcular_custo(temp))
-            no_filhos.put((novo_no.c_n, novo_no))
+            no_filhos.put((novo_no.custo_parcial, novo_no))
 
         if (vazio_j - 1) >= 0:
             temp = np.copy(estado_atual.tabuleiro)
             temp[vazio_i[0]][vazio_j[0]] = temp[vazio_i[0]][vazio_j[0] - 1]
             temp[vazio_i[0]][vazio_j[0] - 1] = 0
             novo_no = No(temp, estado_atual, self._calcular_custo(temp))
-            no_filhos.put((novo_no.c_n, novo_no))
+            no_filhos.put((novo_no.custo_parcial, novo_no))
 
         if (vazio_j + 1) < self.TABULEIRO_FINAL.shape[0]:
             temp = np.copy(estado_atual.tabuleiro)
             temp[vazio_i[0]][vazio_j[0]] = temp[vazio_i[0]][vazio_j[0] + 1]
             temp[vazio_i[0]][vazio_j[0] + 1] = 0
             novo_no = No(temp, estado_atual, self._calcular_custo(temp))
-            no_filhos.put((novo_no.c_n, novo_no))
+            no_filhos.put((novo_no.custo_parcial, novo_no))
 
         return no_filhos
 
@@ -99,4 +102,88 @@ class BuscaCustoUniforme(object):
                         ja_visitado = True
                         break
                 if not ja_visitado:
-                    self.nao_visitados.put((temp.c_n, temp))
+                    self.nao_visitados.put((temp.custo_parcial, temp))
+
+
+class AEstrela(object):
+    TABULEIRO_FINAL = np.array([[1, 2, 3],
+                                [8, 0, 4],
+                                [7, 6, 5]])
+
+    def __init__(self, tabuleiro_inicial):
+        no_inicial = No(tabuleiro_inicial, None, 0, self._custo_heuristico(tabuleiro_inicial))
+
+        self.visitados = []
+        self.nao_visitados = PriorityQueue()
+        self.nao_visitados.put((no_inicial.custo_total, no_inicial))
+
+    def _custo_heuristico(self, temp):
+        return 9 - np.sum((self.TABULEIRO_FINAL == temp).astype(int))
+
+    def _final_jogo(self, tabuleiro_atual):
+        return np.all(self.TABULEIRO_FINAL == tabuleiro_atual)
+
+    def _get_filhos(self, estado_atual):
+        vazio_i, vazio_j = np.where(estado_atual.tabuleiro == 0)
+        no_filhos = PriorityQueue()
+
+        if (vazio_i - 1) >= 0:
+            temp = np.copy(estado_atual.tabuleiro)
+            temp[vazio_i[0]][vazio_j[0]] = temp[vazio_i[0] - 1][vazio_j[0]]
+            temp[vazio_i[0] - 1][vazio_j[0]] = 0
+
+            novo_no = No(temp, estado_atual, 1, self._custo_heuristico(temp))
+            no_filhos.put((novo_no.custo_total, novo_no))
+
+        if (vazio_i + 1) < self.TABULEIRO_FINAL.shape[0]:
+            temp = np.copy(estado_atual.tabuleiro)
+            temp[vazio_i[0]][vazio_j[0]] = temp[vazio_i[0] + 1][vazio_j[0]]
+            temp[vazio_i[0] + 1][vazio_j[0]] = 0
+
+            novo_no = No(temp, estado_atual, 1, self._custo_heuristico(temp))
+            no_filhos.put((novo_no.custo_total, novo_no))
+
+        if (vazio_j - 1) >= 0:
+            temp = np.copy(estado_atual.tabuleiro)
+            temp[vazio_i[0]][vazio_j[0]] = temp[vazio_i[0]][vazio_j[0] - 1]
+            temp[vazio_i[0]][vazio_j[0] - 1] = 0
+
+            novo_no = No(temp, estado_atual, 1, self._custo_heuristico(temp))
+            no_filhos.put((novo_no.custo_total, novo_no))
+
+        if (vazio_j + 1) < self.TABULEIRO_FINAL.shape[0]:
+            temp = np.copy(estado_atual.tabuleiro)
+            temp[vazio_i[0]][vazio_j[0]] = temp[vazio_i[0]][vazio_j[0] + 1]
+            temp[vazio_i[0]][vazio_j[0] + 1] = 0
+
+            novo_no = No(temp, estado_atual, 1, self._custo_heuristico(temp))
+            no_filhos.put((novo_no.custo_total, novo_no))
+
+        return no_filhos
+
+    def buscar_solucao(self):
+        while True:
+            if self.nao_visitados.empty():
+                return "ERRO"
+
+            _, estado_atual = self.nao_visitados.get()
+            self.visitados.append(estado_atual)
+
+            if self._final_jogo(estado_atual.tabuleiro):
+                pilha_nos = [estado_atual]
+                while estado_atual.no_pai is not None:
+                    estado_atual = estado_atual.no_pai
+                    pilha_nos.append(estado_atual)
+                return pilha_nos
+
+            no_filhos = self._get_filhos(estado_atual)
+
+            while not no_filhos.empty():
+                _, temp = no_filhos.get()
+                ja_visitado = False
+                for no in self.visitados:
+                    if np.all(no.tabuleiro == temp.tabuleiro) and (no.custo_total < temp.custo_total):
+                        ja_visitado = True
+                        break
+                if not ja_visitado:
+                    self.nao_visitados.put((temp.custo_total, temp))
--- quebra_cabeca_8pecas.py
+++ quebra_cabeca_8pecas.py
@@ -24,15 +24,21 @@ def main():
             else:
                 conf_inicial = passos.pop()
                 tabuleiro_inicial = qc.montar_tabuleiro(conf_inicial.tabuleiro)
-                cont = 1
-                while passos:
-                    time.sleep(.05)
+                if not passos:
                     limpar_tela()
                     print("Configuração inicial:\n{:s}\n\n{:s}\n".format(tabuleiro_inicial, ('#' * 13)))
-                    temp = passos.pop()
-                    print("Configuração {:d}:\n{:s}\n\n{:s}\n".format(cont, qc.montar_tabuleiro(temp.tabuleiro), ('#' * 13)))
-                    input("Pressione qualquer tecla para continuar.\n")
-                    cont += 1
+                    print("Configuração {:d}:\n{:s}\n\n{:s}\n".format(1, tabuleiro_inicial, ('#' * 13)))
+                    input("Pressione ENTER para continuar.\n")
+                else:
+                    cont = 1
+                    while passos:
+                        time.sleep(.05)
+                        limpar_tela()
+                        print("Configuração inicial:\n{:s}\n\n{:s}\n".format(tabuleiro_inicial, ('#' * 13)))
+                        temp = passos.pop()
+                        print("Configuração {:d}:\n{:s}\n\n{:s}\n".format(cont, qc.montar_tabuleiro(temp.tabuleiro), ('#' * 13)))
+                        input("Pressione ENTER para continuar.\n")
+                        cont += 1
             resposta = 'inf'
             while resposta != 's' and resposta != 'n':
                 resposta = input("Resolver outro quebra cabeça? (s ou n): ")
